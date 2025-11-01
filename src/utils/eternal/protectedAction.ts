4fbb1c0a5352dd7a7892cbdfa45a8d36264e147b
Magician <pranshuchawla19@gmail.com>
2025-11-01 16:28:37 -0500

    fix: deploy fix

--- src/utils/eternal/protectedAction.ts
+++ src/utils/eternal/protectedAction.ts
@@ -1,14 +1,14 @@
-import {Session} from "next-auth";
-import {ShowToast} from "./showToast";
+import type { Session } from "next-auth";
+import type { ShowToast } from "./showToast";
 
 export function protectedAction(
-    session: Session | null | undefined,
-    showToast: ShowToast,
-    message = "Please sign in and upload a My Clippings.txt file."
+  session: Session | null | undefined,
+  showToast: ShowToast,
+  message = "Please sign in and upload a My Clippings.txt file."
 ) {
-    if (!session) {
-        showToast(message);
-        return false;
-    }
-    return true;
+  if (!session) {
+    showToast(message);
+    return false;
+  }
+  return true;
 }
